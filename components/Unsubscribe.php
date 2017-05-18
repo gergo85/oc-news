73107d5d8442489dee81ccb1c2cfc7b02ec257d8
Szabó Gergő <gergo85@gmail.com>
2017-05-18 13:22:20 +0200

    Fixed the translation issue

--- components/Unsubscribe.php
+++ components/Unsubscribe.php
@@ -2,6 +2,7 @@
 
 use Cms\Classes\ComponentBase;
 use Indikator\News\Models\Subscribers;
+use Lang;
 use Validator;
 use ValidationException;
 use Response;
@@ -18,18 +19,22 @@ class Unsubscribe extends ComponentBase
         ];
     }
 
+    public function onRun()
+    {
+        $this->page['text_messages'] = Lang::get('indikator.news::lang.messages.unsubscribed');
+        $this->page['text_email']    = Lang::get('indikator.news::lang.form.email');
+        $this->page['text_button']   = Lang::get('indikator.news::lang.button.unsubscribe');
+    }
+
     public function onUnsubscribe()
     {
         $data = post();
-
         $subscriber = Subscribers::email($data['email'])->first();
 
-        if($subscriber === null || !$subscriber->isActive())
-        {
-            return Response::make(trans('indikator.news::lang.ajax.messages.not_subscribed'), 400);
+        if ($subscriber === null || !$subscriber->isActive()) {
+            return Response::make(Lang::get('indikator.news::lang.messages.not_subscribed'), 400);
         }
 
         $subscriber->unsubscribe();
-
     }
 }
