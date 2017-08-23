d93a61bdeba8a092fa03f2942ce820166a97666c
Tim Förster <mail@tim-foerster.de>
2017-08-23 19:35:20 +0200

    don't queue test email

--- classes/NewsSender.php
+++ classes/NewsSender.php
@@ -31,11 +31,17 @@ class NewsSender
     protected $templateNamespace = 'indikator.news::mail.email_';
 
     /**
+     * @var bool if the sending should be queued.
+     */
+    protected $queued;
+
+    /**
      * NewsSender constructor
      *
      * @param $news Posts should be send.
+     * @param $queued boolean if the email sending should be queued
      */
-    public function __construct($news)
+    public function __construct($news, $queued = true)
     {
         $this->news = $news;
 
@@ -43,6 +49,7 @@ class NewsSender
         if ($pluginManager && !$pluginManager->disabled) {
             $this->locale = true;
         }
+        $this->queued = $queued;
     }
 
     /**
@@ -127,11 +134,19 @@ class NewsSender
             return;
         }
 
-        Queue::push('\Indikator\News\Classes\SendNews', [
-            'template' => $template,
-            'params' => $params,
-            'receiver' => $receiver,
-            'subject' => $this->news->title
-        ]);
+        if($this->queued) {
+            Queue::push('\Indikator\News\Classes\SendNews', [
+                'template' => $template,
+                'params' => $params,
+                'receiver' => $receiver,
+                'subject' => $this->news->title
+            ], 'newsletter');
+        } else {
+            Mail::send($template, $params, function($message) use ($receiver)
+            {
+                $message->to($receiver->email, $receiver->name)->subject($this->news->title);
+            });
+        }
+
     }
 }
