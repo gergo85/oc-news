88056374051dd9ad4162ca118d6f6fec605d9fe1
Tim Förster <mail@tim-foerster.de>
2018-09-24 19:21:10 +0200

    implements feature #74

--- controllers/Posts.php
+++ controllers/Posts.php
@@ -207,4 +207,14 @@ class Posts extends Controller
 
         return $this->makePartial('show_stat');
     }
+
+
+    public function formExtendFields($form)
+    {
+        if ($form->context == 'create') {
+            $form->removeField('newsletter_content');
+            $form->removeField('enable_newsletter_content');
+        }
+
+    }
 }
